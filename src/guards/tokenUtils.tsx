2225a790c95d75aed6c3dc454486b6c7f39b7e7e
mjsimo99 <mohashark21@gmail.com>
2024-04-02 11:58:55 +0000

    fix tocken

--- src/guards/tokenUtils.tsx
+++ src/guards/tokenUtils.tsx
@@ -9,6 +9,12 @@ export interface DecodedToken {
   username: string;
   email: string;
   avatar: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  address: string;
+  clientId: number;
+  
 }
 
 export const getDecodedToken = (token: string): DecodedToken => {
