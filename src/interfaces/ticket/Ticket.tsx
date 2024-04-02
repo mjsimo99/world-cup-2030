4f6462d95ceb96d6037d18e71ac67fc2e7e9e148
mjsimo99 <mohashark21@gmail.com>
2024-04-02 11:59:04 +0000

    fix tickets

--- src/interfaces/ticket/Ticket.tsx
+++ src/interfaces/ticket/Ticket.tsx
@@ -2,10 +2,13 @@ import Client from "../client/Client";
 import Match from "../match/match";
 
 interface Ticket {
-    ticketId:number;
-    quantity:number;
-    client:Client;
+    ticketId: number;
+    quantity: number;
+    client: Client;
     match: Match;
+    clientId: number;
+    matchId: number;
+    totalPrice: number;
 }
 
 export default Ticket;
