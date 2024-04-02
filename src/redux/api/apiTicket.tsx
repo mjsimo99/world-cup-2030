3a536778ae26dba1f1c6076e47d5a48fdd59352f
mjsimo99 <mohashark21@gmail.com>
2024-04-02 11:52:37 +0000

    implement sweet alert in ticket

--- src/redux/api/apiTicket.tsx
+++ src/redux/api/apiTicket.tsx
@@ -1,5 +1,6 @@
 //apiTicket.tsx
 import axios from 'axios';
+import { showErrorAlert, showSuccessAlert } from '../../interceptor/sweetAlertUtils';
 const API_URL = 'http://localhost:8080/api/tickets';
 
 const ApiTicket = {
@@ -15,30 +16,46 @@ const ApiTicket = {
     async deleteTicket(ticketId: number) {
         try {
             await axios.delete(`${API_URL}/${ticketId}`);
-        }
-        catch (error) {
-            return error;
+            showSuccessAlert('Ticket deleted', `Ticket deleted successfully`);
+        }  catch (error : any) {
+            const errorData = error.response.data;
+            const errorMessages = Object.values(errorData).join(' + ');
+            showErrorAlert('Error deleting ticket', errorMessages);
         }
     },
 
     async createTicket(ticketData: any) {
         try {
             const response = await axios.post(API_URL, ticketData);
+            showSuccessAlert('Ticket bought!', 'The ticket has been bought.');
             return response.data;
-        } catch (error) {
-            return error;
+        }  catch (error : any) {
+            const errorData = error.response.data;
+            const errorMessages = Object.values(errorData).join(' + ');
+            showErrorAlert('Error buying ticket', errorMessages);
         }
     },
 
     async updateTicket(ticketId: number, ticketData: any) {
         try {
-            await axios.put(`${API_URL}/${ticketId}`, ticketData
-            );
+            await axios.put(`${API_URL}/${ticketId}`, ticketData);
+            showSuccessAlert('Ticket updated', `Ticket updated successfully`);
+        }  catch (error : any) {
+            const errorData = error.response.data;
+            const errorMessages = Object.values(errorData).join(' + ');
+            showErrorAlert('Error updating ticket', errorMessages);
         }
-        catch (error) {
+    },
+    async fetchTicketsByClientId(clientId: number) {
+        try {
+            const response = await axios.get(`${API_URL}/clients/${clientId}`);
+            return response.data;
+        } catch (error) {
             return error;
         }
     }
+    
 };
+
 
 export default ApiTicket;
