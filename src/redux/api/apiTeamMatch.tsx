37369ff8f14da1651cd9604281a02e49f275e29e
mjsimo99 <mohashark21@gmail.com>
2024-04-02 11:52:28 +0000

    implement sweet alert in teams-matches

--- src/redux/api/apiTeamMatch.tsx
+++ src/redux/api/apiTeamMatch.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import TeamMatch from '../../interfaces/team-match/TeamMatch';
+import { showErrorAlert, showSuccessAlert } from '../../interceptor/sweetAlertUtils';
 
 const API_URL = 'http://localhost:8080/api/team-matches';
 
@@ -18,26 +19,35 @@ const ApiTeamMatch = {
     async createTeamMatch(teamMatchData: TeamMatch) {
         try {
             const response = await axios.post(API_URL, teamMatchData);
-            return response.data; 
-        } catch (error) {
-            return error;
+            showSuccessAlert('Team Match created', `Team Match created successfully`);
+            return response.data;
+        } catch (error : any) {
+            const errorData = error.response.data;
+            const errorMessages = Object.values(errorData).join(' + ');
+            showErrorAlert('Error creating team match', errorMessages);
         }
     },
     async deleteTeamMatch(teamId: number, matchId: number) {
         try {
-            const response = await axios.delete(`${API_URL}/${teamId}/${matchId}`); // Ensure that teamId and matchId are used to form the API endpoint
+            const response = await axios.delete(`${API_URL}/${teamId}/${matchId}`);
+            showSuccessAlert('Team Match deleted', `Team Match deleted successfully`);
             return response.data;
-        } catch (error) {
-            return error;
+        }  catch (error : any) {
+            const errorData = error.response.data;
+            const errorMessages = Object.values(errorData).join(' + ');
+            showErrorAlert('Error deleting team match', errorMessages);
         }
     },
-    
+
     async updateTeamMatch(teamId: number, matchId: number, teamMatchData: TeamMatch) {
         try {
-            const response = await axios.put(`${API_URL}/${teamId}/${matchId}`, teamMatchData); // Include teamMatchData in the request body
+            const response = await axios.put(`${API_URL}/${teamId}/${matchId}`, teamMatchData);
+            showSuccessAlert('Team Match updated', `Team Match updated successfully`);
             return response.data;
-        } catch (error) {
-            return error;
+        }  catch (error : any) {
+            const errorData = error.response.data;
+            const errorMessages = Object.values(errorData).join(' + ');
+            showErrorAlert('Error updating team match', errorMessages);
         }
     }
 };
