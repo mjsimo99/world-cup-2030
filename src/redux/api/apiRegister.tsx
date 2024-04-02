4d15c8c87b4a4b8a016a31f66932bef0c6306761
mjsimo99 <mohashark21@gmail.com>
2024-04-02 11:51:59 +0000

    implement sweet alert in register

--- src/redux/api/apiRegister.tsx
+++ src/redux/api/apiRegister.tsx
@@ -1,14 +1,18 @@
 import axios from 'axios';
-
+import { showErrorAlert, showSuccessAlert } from '../../interceptor/sweetAlertUtils';
+import Client from '../../interfaces/client/Client';
 const API_URL = 'http://localhost:8080/client/register';
 
 const ApiRegister = {
-    async register(registerData: any) {
+    async register(registerData: Client) {
         try {
             const response = await axios.post(API_URL, registerData);
+            showSuccessAlert('Client registered', 'Registration successful , please login to continue.');
             return response.data;
-        } catch (error) {
-            return error;
+        }  catch (error : any) {
+            const errorData = error.response.data;
+            const errorMessages = Object.values(errorData).join(' + ');
+            showErrorAlert('Error registering client', errorMessages);
         }
     }
 };
