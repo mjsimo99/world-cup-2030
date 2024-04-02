fe49fea474b083e0a7777bd3859cb39fcec271af
mjsimo99 <mohashark21@gmail.com>
2024-04-02 11:51:41 +0000

    implement sweet alert in city

--- src/redux/api/apiCity.tsx
+++ src/redux/api/apiCity.tsx
@@ -1,5 +1,7 @@
 //apiCity.tsx
 import axios from 'axios';
+import { showErrorAlert, showSuccessAlert } from '../../interceptor/sweetAlertUtils';
+import City from '../../interfaces/city/City';
 const API_URL = 'http://localhost:8080/api/cities';
 
 const ApiCity = {
@@ -15,28 +17,30 @@ const ApiCity = {
     async deleteCity(cityId: number) {
         try {
             await axios.delete(`${API_URL}/${cityId}`);
+            showSuccessAlert('City deleted', `City deleted successfully`);
         }
         catch (error) {
             return error;
         }
     },
 
-    async createCity(cityData: any) {
+    async createCity(cityData: City) {
         try {
             const response = await axios.post(API_URL, cityData);
+            showSuccessAlert('City created', `City ${response.data.name} created successfully`);
             return response.data;
-        } catch (error) {
-            return error;
+        } catch (error : any) {
+            showErrorAlert('Error creating city',error.response.data.name);
         }
     },
 
-    async updateCity(cityId: number, cityData: any) {
+    async updateCity(cityId: number, cityData: City) {
         try {
-            await axios.put(`${API_URL}/${cityId}`, cityData
-            );
+            await axios.put(`${API_URL}/${cityId}`, cityData);
+            showSuccessAlert('City updated', `City ${cityData.name} updated successfully`);
         }
-        catch (error) {
-            return error;
+        catch (error : any) {
+            showErrorAlert('Error updating city', error.response.data.name);
         }
     }
 };
