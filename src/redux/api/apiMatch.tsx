03b4f2c1cc0524b527e2a0c81c67e38bee094d47
mjsimo99 <mohashark21@gmail.com>
2024-04-02 11:51:53 +0000

    implement sweet alert in match

--- src/redux/api/apiMatch.tsx
+++ src/redux/api/apiMatch.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { showErrorAlert, showSuccessAlert } from '../../interceptor/sweetAlertUtils';
 
 const API_URL = 'http://localhost:8080/api/matches';
 
@@ -15,23 +16,40 @@ const ApiMatch = {
     async deleteMatch(matchId: number) {
         try {
             await axios.delete(`${API_URL}/${matchId}`);
-        } catch (error) {
-            return error;
+            showSuccessAlert('Match deleted', `Match deleted successfully`);
+        }  catch (error : any) {
+            const errorData = error.response.data;
+            const errorMessages = Object.values(errorData).join(' + ');
+            showErrorAlert('Error deleting match', errorMessages);
         }
     },
 
     async createMatch(matchData: any) {
         try {
             const response = await axios.post(API_URL, matchData);
+            showSuccessAlert('Match created', `Match ${response.data.name} created successfully`);
             return response.data; 
-        } catch (error) {
-            return error;
+        } catch (error : any) {
+            const errorData = error.response.data;
+            const errorMessages = Object.values(errorData).join(' + ');
+            showErrorAlert('Error creating match', errorMessages);
         }
     },
 
     async updateMatch(matchId: number, matchData: any) {
         try {
             await axios.put(`${API_URL}/${matchId}`, matchData);
+            showSuccessAlert('Match updated', `Match ${matchData.name} updated successfully`);
+        }  catch (error : any) {
+            const errorData = error.response.data;
+            const errorMessages = Object.values(errorData).join(' + ');
+            showErrorAlert('Error updating match', errorMessages);
+        }
+    },
+    async getMatchById(matchId: number) {
+        try {
+            const response = await axios.get(`${API_URL}/${matchId}`);
+            return response.data;
         } catch (error) {
             return error;
         }
