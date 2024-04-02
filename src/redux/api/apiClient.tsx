f0720e4da27eea7641e6822e1011d627d4968ceb
mjsimo99 <mohashark21@gmail.com>
2024-04-02 11:51:47 +0000

    implement sweet alert in client

--- src/redux/api/apiClient.tsx
+++ src/redux/api/apiClient.tsx
@@ -1,5 +1,7 @@
 //apiCLinet.tsx
 import axios from 'axios';
+import { showErrorAlert, showSuccessAlert } from '../../interceptor/sweetAlertUtils';
+import Client from '../../interfaces/client/Client';
 const API_URL = 'http://localhost:8080/api/clients';
 
 const ApiClient = {
@@ -15,19 +17,22 @@ const ApiClient = {
     async deleteClient(clientId: number) {
         try {
             await axios.delete(`${API_URL}/${clientId}`);
-        }
-        catch (error) {
-            return error;
+            showSuccessAlert('Client deleted', `Client deleted successfully`);
+        } catch (error : any) {
+            const errorData = error.response.data;
+            const errorMessages = Object.values(errorData).join(' + ');
+            showErrorAlert('Error deleting client', errorMessages);
         }
     },
 
-    async updateClient(clientId: number, clientData: any) {
+    async updateClient(clientId: number, clientData: Client) {
         try {
-            await axios.put(`${API_URL}/${clientId}`, clientData
-            );
-        }
-        catch (error) {
-            return error;
+            await axios.put(`${API_URL}/${clientId}`, clientData);
+            showSuccessAlert('Client updated', `Client ${clientData.firstName} ${clientData.lastName} updated successfully`);
+        } catch (error : any) {
+            const errorData = error.response.data;
+            const errorMessages = Object.values(errorData).join(' + ');
+            showErrorAlert('Error updating client', errorMessages);
         }
     }
 }
