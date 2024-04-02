16e13346a66a7d72cb2e41b2be2e303137960981
mjsimo99 <mohashark21@gmail.com>
2024-04-02 11:58:07 +0000

    make sweet alert usable

--- src/interceptor/sweetAlertUtils.tsx
+++ src/interceptor/sweetAlertUtils.tsx
@@ -20,3 +20,41 @@ export const showSuccessAlert = (title : string, text : string) => {
         confirmButtonColor: '#3085d6',
     });
 };
+
+export const showErrorAlert = (title : string, text : string) => {
+    Swal.fire({
+        title: title,
+        text: text,
+        icon: 'error',
+        confirmButtonColor: '#3085d6',
+    });
+};
+
+export const showInfoAlert = (title : string, text : string) => {
+    Swal.fire({
+        title: title,
+        text: text,
+        icon: 'info',
+        confirmButtonColor: '#3085d6',
+    });
+};
+
+export const showWarningAlert = (title : string, text : string) => {
+    Swal.fire({
+        title: title,
+        text: text,
+        icon: 'warning',
+        confirmButtonColor: '#3085d6',
+    });
+};
+export const showCancelButtonAlert = (title : string, text : string) => {
+    Swal.fire({
+        title: title,
+        text: text,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, confirm!',
+    });
+}
